e3a9c83e422d51e0b2821284a5d05c92dce4c77f
Brian Snook <1421890+bmsnook@users.noreply.github.com>
2024-03-06 18:15:44 -0500

    import api key and secret from AWS secrets manager instead of hardcoding

--- dyndns_update.sh
+++ dyndns_update.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
 
-## 
-## Credits:
-## Quick and Dirty Dynamic DNS Using GoDaddy by Tod-SoS
-## https://www.instructables.com/Quick-and-Dirty-Dynamic-DNS-Using-GoDaddy/
-## https://www.instructables.com/member/Tod-SoS/
-## 
-
 mydomain="yourdomain.dom"
 myhostname="gateway"
-gdapikey="api_key:key_secret"
+host1="host1"
+host2="host2"
 logdest="local7.info"
+gdapikey="$(/usr/local/bin/aws secretsmanager get-secret-value \
+            --secret-id prod/dns/godaddy \
+            --query SecretString | \
+                jq -r '[.dns_api_key, .dns_api_secret] | join(":")'
+           )"
 
-myip=`curl -s "https://api.ipify.org"`
-dnsdata=`curl -s -X GET -H "Authorization: sso-key ${gdapikey}" "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}"`
-gdip=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
-echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $myip, GoDaddy DNS IP is $gdip"
+datestamp_msg() {
+    DATETIME="$(date "+%Y-%m-%d %H:%M:%S")"
+    printf "${DATETIME} DNS-UPDATE: ${@}\n"
+}
+cur_ip="$(/usr/bin/curl -s "https://api.ipify.org")"
+dnsdata="$(/usr/bin/curl -s \
+            -X GET \
+            -H "Authorization: sso-key ${gdapikey}" \
+            "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}"
+          )"
 
-if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
-  echo "IP has changed!! Updating on GoDaddy"
-  curl -s -X PUT "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}" -H "Authorization: sso-key ${gdapikey}" -H "Content-Type: application/json" -d "[{\"data\": \"${myip}\"}]"
-  logger -p $logdest "Changed IP on ${myhostname}.${mydomain} from ${gdip} to ${myip}"
+#dns_ip="$(echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2)"
+#dns_ip="$(echo $dnsdata | awk -F'[:,]' '{ip=gensub("[][{}\"]","","g",$2);print ip}')"
+#dns_ip="$(echo $dnsdata | awk -F'[:,]' '{ip=gensub("\"","","g",$2);print ip}')"
+#dns_ip="$(echo $dnsdata | awk -F'[:,]' '{gsub(/"/,"");print $2}')"
+dns_ip="$(printf ${dnsdata} | jq -r '.[].data')"
+#echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $cur_ip, GoDaddy DNS IP is $dns_ip"
+datestamp_msg "hostname ${myhostname}.${mydomain} current IP: ${cur_ip} and DNS IP: ${dns_ip}"
+
+if [ "$dns_ip" != "$cur_ip" -a "$cur_ip" != "" ]; then
+    printf "IP has changed! Updating GoDaddy from \"${dns_ip}\" to \"${cur_ip}\"\n"
+    ## gateway
+    /usr/bin/curl -s -X PUT \
+        "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}" \
+        -H "Authorization: sso-key ${gdapikey}" \
+        -H "Content-Type: application/json" \
+        -d "[{\"data\": \"${cur_ip}\"}]" && \
+    logger -p $logdest "SUCCESS: Changed IP for ${myhostname}.${mydomain} from \"${dns_ip}\" to \"${cur_ip}\"" || \
+    logger -p $logdest "FAILURE: ERROR updating ${myhostname}.${mydomain} from \"${dns_ip}\" to \"${cur_ip}\""
+    ## host1
+    /usr/bin/curl -s -X PUT \
+        "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${host1}" \
+        -H "Authorization: sso-key ${gdapikey}" \
+        -H "Content-Type: application/json" \
+        -d "[{\"data\": \"${cur_ip}\"}]" && \
+    logger -p $logdest "SUCCESS: Changed IP for ${host1}.${mydomain} from \"${dns_ip}\" to \"${cur_ip}\"" || \
+    logger -p $logdest "FAILURE: ERROR updating ${host1}.${mydomain} from \"${dns_ip}\" to \"${cur_ip}\""
+    ## host2
+    /usr/bin/curl -s -X PUT \
+        "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${host2}" \
+        -H "Authorization: sso-key ${gdapikey}" \
+        -H "Content-Type: application/json" \
+        -d "[{\"data\": \"${cur_ip}\"}]" && \
+    logger -p $logdest "SUCCESS: Changed IP for ${host2}.${mydomain} from \"${dns_ip}\" to \"${cur_ip}\"" || \
+    logger -p $logdest "FAILURE: ERROR updating ${host2}.${mydomain} from \"${dns_ip}\" to \"${cur_ip}\""
 fi
+
